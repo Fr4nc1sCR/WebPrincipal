3a6c1dab79845ca39cf63bc4d7cc106cf3c23f26
Julian Quesada Víquez <julian.quesadaviquez@eagles.oc.edu>
2025-06-09 18:39:40 -0600

    Add files via upload

---
+++ FreeLancer - Comienzo/javascript/funciones.js
@@ -0,0 +1,9 @@
+function mostrarEncabezado() {
+    header.classList.remove('oculto');
+    header.classList.add('visible');
+}
+
+function ocultarEncabezado() {
+    header.classList.add('oculto');
+    header.classList.remove('visible');
+}
