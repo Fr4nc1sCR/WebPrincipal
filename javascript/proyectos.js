9e480eeaecc26c7345ca97f45760eaae42040e29
Julian Quesada Víquez <julian.quesadaviquez@eagles.oc.edu>
2025-07-31 17:45:55 -0600

    Entre Lineas Lib!

--- javascript/proyectos.js
+++ javascript/proyectos.js
@@ -2,8 +2,14 @@
 const proyectos = [
   {
     titulo: "Side Street",
-    url: "https://sidestreet.netlify.app/",
+    url: "https://sidestreet.netlify.app",
     imagen: "img/sidestreet-preview.png",
     descripcion: "Local de Pizza Urbana con diseño moderno y enfoque visual minimalista."
+  },
+  {
+    titulo: "Entre Líneas",
+    url: "https://entrelineaslib.netlify.app",
+    imagen: "img/entrelineaslib.png",
+    descripcion: "Una librería virtual moderna, con catálogos actualizados, navegación intuitiva y opciones seguras de compra."
   }
 ];
